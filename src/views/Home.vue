cf43b1f5e8375d38e57bc749e60400a7a4f948a7
dani <dani.zaghian@gmail.com>
2019-03-04 14:07:29 -0800

    adds cat logic

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,41 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ message }}</h1>
+    <h3>What is your cat's calendar year age?</h3>
+    <input type="number" v-model="realAge">
+
+    <div v-if="realAge">
+    	<h4>Your cat in human years is: {{ calculateCatAge }}</h4>
+    </div>
+
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<style>
+</style>
 
+<script>
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  data: function() {
+    return {
+      message: "Cat Age Calculator",
+      realAge: "",
+      catAge: ""
+    };
+  },
+  created: function() {},
+  methods: {},
+  computed: {
+  	calculateCatAge: function() {
+  		if (this.realAge == 1) {
+  			this.catAge = 15;
+  		} else if (this.realAge == 2) {
+  			this.catAge = 24;
+  		} else {
+  			this.catAge = ((this.realAge - 2) * 4) + 24;
+  		}
+  		return this.catAge;
+  	}
   }
-}
+};
 </script>
